5bb858e97c4943d3d3c5ac71cb2597e0f678df72
Harshil <harshilgupta.2005@gmail.com>
2025-08-18 00:52:39 +0530

    added request logger

--- src/utils/mongooseClient.ts
+++ src/utils/mongooseClient.ts
@@ -5,7 +5,6 @@ export const connectMongo = async () => {
 	try {
 		await mongoose.connect(config.database.uri);
 		console.log("Connected to MongoDB");
-		console.log(`Database: ${config.database.uri}`);
 	} catch (error) {
 		console.error("MongoDB connection error:", error);
 		process.exit(1);
